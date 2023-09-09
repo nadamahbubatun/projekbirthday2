596aa2067ea7aec107990ab31fcbbb4b170d457a
unknown <nadamahbubatun@gmail.com>
2023-09-09 20:44:29 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+let stars = document.getElementById("stars");
+let meteorid = document.getElementById("meteorid");
+let rocket = document.getElementById("rocket");
+let text = document.getElementById("text");
+let button = document.getElementById("button");
+
+
+window.addEventListener('scroll', () => {
+  let value = window.scrollY;
+
+  text.style.marginTop = value + 15 + "px";
+  button.style.marginTop = value + 15 + "px";
+});
+
+
+// Css nya jangan ngasal 
+
+// window.addEventListener(scroll, function () {
+//   let value = window.scrollY;
+//   stars.style.left = value * 0.25 + "px";
+// });
